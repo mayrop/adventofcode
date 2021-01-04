0b9d38440111f9cc8f2b37d7db8d5949c966d261
Mayra Valdes <4valdes@gmail.com>
2021-01-03 20:17:07 -0600

    refactor: refactor code for day 4 in julia

--- julia/day04.jl
+++ julia/day04.jl
@@ -1,4 +1,5 @@
 using DataFrames
+using DataFramesMeta
 
 #-------------------------------------------------------
 
@@ -8,16 +9,18 @@ file = read(f, String)
 # few file operations
 close(f)
 
+# https://datascience-enthusiast.com/R/R_Julia_cheat_sheet.html
 #-------------------------------------------------------
 lines = split(file, "\n\n")
 lines = map(line -> replace(line, "\n" => " "), lines)
 
 df = DataFrame(raw=lines)
-df[!, :is_valid] .= false
 
 # Regex: https://gist.github.com/dataPulverizer/23c8d992d351d7faf0ed1c1966605b10
 foreach([:pid, :hgt, :byr, :iyr, :eyr, :hcl, :ecl]) do (col)
-   df[!, col] .= ""
+   df[!, col] .= missing
+   # https://discourse.julialang.org/t/replacing-missing-values-in-dataframe-convert-type-union-float64-is-ambiguous/52119
+   df[!, col] = convert(Vector{Union{String, Missing}}, df[!, col])
 
    foreach(1:size(df, 1)) do (i)
       col_match = match(Regex("$(col):([^ ]+)"), df[i, :raw])
@@ -27,16 +30,16 @@ foreach([:pid, :hgt, :byr, :iyr, :eyr, :hcl, :ecl]) do (col)
    end
 end
 
-foreach(1:size(df, 1)) do (i)
-   line = df[i, :]
-   # How to clean this?
-   is_valid = (line.pid != "" && line.hgt != "" && line.byr != "" && line.iyr != ""
-      && line.eyr != "" && line.hcl != "" && line.ecl != "")
-   df[i, :is_valid] = is_valid
-end
-
 #-------------------------------------------------------
 
 # Part 1
-dims = filter(line -> line.is_valid, df) |> size
+dims = dropmissing(df) |> size
 dims[1]
+
+
+#-------------------------------------------------------
+
+# For later
+# https://stackoverflow.com/questions/58672335/julia-quickly-make-a-column-wise-sum-over-an-array
+# https://stackoverflow.com/questions/58672335/julia-quickly-make-a-column-wise-sum-over-an-arraymatr
+# https://www.juliabloggers.com/data-wrangling-in-julia-based-on-dplyr-flights-tutorials/
